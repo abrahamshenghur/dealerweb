f3a68a871ad2adcca8492b09cb54e88b6f6809f3
abrahamshenghur <ashenghur@gmail.com>
2022-03-16 17:49:29 -0700

    Initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+https://www.youtube.com/watch?v=oYRda7UtuhA
+
+How To Make Website Using HTML & CSS | Full Responsive Multi Page Website Design Step by Step
+
---
+++ index.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("navLinks")
+
+function showMenu() {
+    navLinks.style.right = "0px"
+    console.log("hello");
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px"
+}
